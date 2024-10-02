14f4c76ea821a79eebf54604794e2a6133347cb9
Hphill06 <105803530+Hphill06@users.noreply.github.com>
2024-10-02 10:19:19 -0500

    Create main.py

---
+++ project/main.py
@@ -0,0 +1,6 @@
+#kinda useless way of starting this project but helped me with formatting and 
+#keeping my files in order
+
+from subprocess import call 
+import sys
+call(["python", "chess/chessmain.py"])
